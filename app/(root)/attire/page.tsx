1b36a35f0e5544fc81664a1f94c280a1e146498d
Erwin Antepuesto <66218784+sciyon@users.noreply.github.com>
2024-11-24 15:01:21 +0800

    attire

--- app/(root)/attire/page.tsx
+++ app/(root)/attire/page.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import Image from 'next/image';
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='flex flex-col items-center'>
+      <h1 className='font-cormorant text-6xl py-6'>Dress Code</h1>
+      <div className="flex flex-row pb-12">
+        <div className="flex flex-col items-center">
+          <Image 
+            src='/girl.png' // Path to your SVG file
+            alt='women outfit'
+            width={400} // Set the desired width
+            height={400} // Set the desired height
+          />
+          <h1 className='text-4xl font-crimson'>Filipiniana</h1>
+        </div>
+        <div className="flex flex-col items-center gap-y-6">
+          <div className="bg-[#ffffc5] w-24 h-24"></div>
+          <div className="bg-[#cfb97d] w-24 h-24"></div>
+          <div className="bg-[#b48b1a] w-24 h-24"></div>
+          <div className="bg-[#a6a6a6] w-24 h-24"></div>
+        </div>
+        <div className="flex flex-col items-center">
+          <Image 
+            src='/boy.png' // Path to your SVG file
+            alt='women outfit'
+            width={400} // Set the desired width
+            height={400} // Set the desired height
+          />
+          <h1 className='text-4xl font-crimson'>Barong and Black Slacks</h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
