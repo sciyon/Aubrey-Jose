db01cbf143049bbc5668fdcbb3e317d3ee8c1844
Erwin Antepuesto <66218784+sciyon@users.noreply.github.com>
2024-11-28 21:11:40 +0800

    mobile gifts

--- app/(root)/attire/page.tsx
+++ app/(root)/attire/page.tsx
@@ -7,29 +7,33 @@ const page = () => {
     <div className='flex flex-col items-center'>
       <h1 className={`${cormorant.className} text-6xl py-6`}>Dress Code</h1>
       <div className="flex flex-row pb-12">
-        <div className="flex flex-col items-center">
-          <Image 
-            src='/girl.png' // Path to your SVG file
-            alt='women outfit'
-            width={400} // Set the desired width
-            height={400} // Set the desired height
-          />
-          <h1 className='text-4xl font-crimson'>Filipiniana</h1>
-        </div>
         <div className="flex flex-col items-center gap-y-6">
           <div className="bg-[#ffffc5] w-24 h-24"></div>
           <div className="bg-[#cfb97d] w-24 h-24"></div>
           <div className="bg-[#b48b1a] w-24 h-24"></div>
           <div className="bg-[#a6a6a6] w-24 h-24"></div>
         </div>
-        <div className="flex flex-col items-center">
-          <Image 
-            src='/boy.png' // Path to your SVG file
-            alt='women outfit'
-            width={400} // Set the desired width
-            height={400} // Set the desired height
-          />
-          <h1 className='text-4xl font-crimson'>Barong and Black Slacks</h1>
+        <div className="flex lg:flex-row flex-col">
+          <div className="flex flex-col items-center">
+            <Image 
+              src='/girl.png' // Path to your SVG file
+              alt='women outfit'
+              width={400} // Set the desired width
+              height={400} // Set the desired height
+              className='h-64 w-40 lg:h-3/4 lg:w-80 lg:ml-12'
+            />
+            <h1 className='lg:text-4xl text-2xl font-crimson'>Filipiniana</h1>
+          </div>
+          <div className="flex flex-col items-center">
+            <Image 
+              src='/boy.png' // Path to your SVG file
+              alt='women outfit'
+              width={400} // Set the desired width
+              height={400} // Set the desired height
+              className='h-64 w-40 lg:h-3/4 lg:w-96'
+            />
+            <h1 className='lg:text-4xl text-2xl text-center font-crimson'>Barong and Black Slacks</h1>
+          </div>
         </div>
       </div>
     </div>
--- app/(root)/gifts/page.tsx
+++ app/(root)/gifts/page.tsx
@@ -5,21 +5,21 @@ const page = () => {
   return (
     <div className='flex flex-col items-center'>
       <h1 className={`${cormorant.className} text-6xl py-3`}>Gifts</h1>
-      <div className="pt-2 font-crimson flex flex-col items-center w-1/3 text-center text-2xl">
+      <div className="pt-2 font-crimson flex flex-col items-center lg:w-1/3 text-center text-2xl p-5">
         <p>With hearts full of love, we are blessed.</p>
         <p>Your presence is our only request.</p>
         <p>But should you wish to give more;</p>
         <p>a <strong>monetary gift</strong>, we humbly implore.</p>
       </div>
-      <div className="flex flex-row items-center justify-center mt-2">
+      <div className="flex lg:flex-row flex-col items-center justify-center mt-2">
         <Image 
           src='/gcash.png' // Path to your SVG file
           alt='wedding cake'
           width={400} // Set the desired width
           height={400} // Set the desired height
-          className="mr-12"
+          className="lg:mr-12 w-5/6 lg:h-3/4 lg:w-fit lg:ml-12"
         />
-        <div className="text-3xl flex flex-col items-center font-crimson">
+        <div className="text-3xl flex flex-col items-center font-crimson pb-10">
           <h1><strong>GCash:</strong></h1>
           <h1><strong>Aubrey A.</strong></h1>
           <h1>0917-954-0832</h1>
